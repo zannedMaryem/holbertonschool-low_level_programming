b8f67372622159ce7414c1b46c2c86c202a12933
Maryem <zanned.maryem@gmail.com>
2026-02-11 17:38:11 +0100

    Add 4-puts.c script update 4 with comment on mmain function

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -5,6 +5,8 @@ int main(void)
  *  main - Entry point
  *  Description: Prints "Programming is like building a multilingual puzzle"
  *              followed by a new line using the puts function.
+ *
+ *  Return: Always 0 (Success)
  */
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
