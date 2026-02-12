1e2e722404844ca1a21a96646eab198e8ab26c67
Maryem <zanned.maryem@gmail.com>
2026-02-12 10:50:28 +0100

    Add 0-positive_or_negative.c script

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * Main - Entry point
+ *
+ * Description: This is a program that takes a random value n
+ *		and determines whether if it's positive or negative or null
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is zero", n);
+	}
+	else
+	{
+		printf("%d is negative", n);
+	}
+	return (0);
+}
