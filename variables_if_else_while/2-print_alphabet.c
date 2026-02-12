378e0e485b5202fc035f1b60f1ed61d33d32ff6c
Maryem <zanned.maryem@gmail.com>
2026-02-12 13:32:52 +0100

    Add 2-print_alphabet.c script

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	return (0);
+}
