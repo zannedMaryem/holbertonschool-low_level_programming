34f88a4d47a55141912f0fc9b7e617b05c365f9f
Maryem <zanned.maryem@gmail.com>
2026-02-13 17:33:56 +0100

    Add updates to files main.h _putchar.c and 0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -5,10 +5,7 @@
  * Return: Always 0 (Success)
  * Description: This program prints "_putchar" followed by a new line
  *				to the standard output.
- *@c: The character to be printed
  */
-int _putchar(char c);
-
 int main(void)
 {
 	int i;
--- functions_nested_loops/_putchar.c
+++ functions_nested_loops/_putchar.c
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include <unistd.h>
+
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -8,4 +8,6 @@
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
-#endif
+{
+	return (write(1, &c, 1));
+}
