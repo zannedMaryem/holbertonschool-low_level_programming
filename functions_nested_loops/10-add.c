1ff75bfde5755a109c4331363067dacd6e96cee8
maryem <zanned.maryem@gmail.com>
2026-02-14 17:33:23 +0100

    add 10-add.c file

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds tow integers and returns the result.
+ * @a: The first integer to be added
+ * @b: The second integer to be added
+ * Return: The result of the addition
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
