b62312654c68c672fe17ce1b1b788a127d13761e
maryem <zanned.maryem@gmail.com>
2026-02-19 10:20:29 +0100

    Add 1-swap.c and main.h files

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - swaps the value of two integers
+ * @a: first integr
+ * @b: second integer
+ */
+void swap_int(int *a, int *b)
+{
+	int *c;
+
+	*c = a;
+	*a = b;
+	*b = c;
+}
