e50a42cf51e4c6cc3dc52b164e5e98ff91f91140
maryem <zanned.maryem@gmail.com>
2026-02-19 14:10:58 +0100

    add 5-rev_string.c file

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -7,3 +7,35 @@ void rev_string(char *s)
 {
 	print_rev(s);
 }
+/**
+ *print_rev - a function that prints a string, in reverse,
+ *followed by a new line.
+ * @s: string variable
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i = _strlen(s) - 1;
+
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
+/**
+ * _strlen - Function that returns the length of a string.
+ * @s: string variable
+ * Return: retuns the length oof string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; *s != '\0'; i++)
+	{
+		s++;
+	}
+	return (i);
+}
