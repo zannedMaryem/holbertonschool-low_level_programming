d6ab5259f546211198b529d50d58c1134ff3111c
maryem <zanned.maryem@gmail.com>
2026-02-19 13:53:37 +0100

    Adding betty documentation style to 4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,11 +1,10 @@
 #include "main.h"
 /**
- * print_rev - a function that prints a string, in reverse,
+ *print_rev - a function that prints a string, in reverse,
  *followed by a new line.
- * @s: string to reverse
+ * @s: string variable
+ * Return: void
  */
-void print_rev(char *s);
-
 void print_rev(char *s)
 {
 	int i = _strlen(s) - 1;
@@ -16,15 +15,4 @@ void print_rev(char *s)
 		i--;
 	}
 	_putchar('\n');
-}
-
-int _strlen(char *s)
-{
-	int i;
-
-	for (i = 0; *s != '\0'; i++)
-	{
-		s++;
-	}
-	return (i);
 }
