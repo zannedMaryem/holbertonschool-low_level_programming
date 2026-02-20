b3673e66e1f6edbd78c3ee8a413cd27d4987acbd
maryem <zanned.maryem@gmail.com>
2026-02-20 21:41:54 +0100

    Add 100-atoi.c file

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _atoi - a function that converts a string into an integer
+ * @s: input string
+ *
+ * Return: return 0 if the string has no integers
+ */
+int _atoi(char *s)
+{
+	int i = 0;
+	int result = 0;
+	int sign = 1;
+	while (s[i] == '-' || s[i] == '+')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		result = result * 10 + (s[i] - '0');
+	}
+	return (result * sign);
+}
