a135e70b8deface167dfae731da01fd6dd3999da
maryem <zanned.maryem@gmail.com>
2026-02-20 10:41:05 +0100

    Add 6-puts2.c with updates

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -9,11 +9,13 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (i % 2 != 0)
+	while (str[i] != '\0')
 	{
-		_putchar(str[i]);
-		str++;
-		i++;
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+			i++;
+		}
 	}
 	_putchar('\n');
 }
