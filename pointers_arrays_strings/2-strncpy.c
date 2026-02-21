b91158213a5ac4a098b0491440ecf865b2f202be
maryem <zanned.maryem@gmail.com>
2026-02-21 22:47:08 +0100

    add 2-strncpy.c file

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -10,9 +10,8 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	int len = _strlen(src);
 
-	for (i = 0; i < len && src[i] != '\0'; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
