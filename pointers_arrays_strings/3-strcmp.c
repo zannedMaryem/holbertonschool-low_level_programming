2b99561e2d9c06ffc1be2f2b88e1ad524688b5d9
maryem <zanned.maryem@gmail.com>
2026-02-22 12:37:33 +0100

    add 3-strcmp.c file

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: 0 if equal, negative if s1 < s2, positive if s1 > s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
