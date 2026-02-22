bc68d04bca5d41d723b8a92b27aadb2bd58146b3
maryem <zanned.maryem@gmail.com>
2026-02-22 14:19:58 +0100

    add 7-leet.c file

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * leet - encodes a string into 1337
+ * @s: input string
+ *
+ * Return: pointer to the modified string
+ */
+char *leet(char *s)
+{
+	int i, j;
+	char letters[] = "aAeEoOtTlL";
+	char numbers[] = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; letters[j] != '\0'; j++)
+		{
+			if (s[i] == letters[j])
+			{
+				s[i] = numbers[j];
+				break;
+			}
+		}
+	}
+	return s;
+}
