4e2a18dea70f5d89571d4b034ed832f3576cdf39
maryem <zanned.maryem@gmail.com>
2026-02-24 13:20:22 +0100

    add 3-strspn.c file

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -17,8 +17,10 @@ unsigned int _strspn(char *s, char *accept)
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
+			{
 				found = 1;
-			break;
+				break;
+			}
 		}
 		if (found)
 			k++;
