3ab0d76d2a4558efb628b21b80b2562ee14c0895
maryem <zanned.maryem@gmail.com>
2026-03-01 17:07:04 +0100

    5-sqrt_recursion.c new version

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,31 +1,36 @@
 #include "main.h"
 /**
- * _sqrt_recursion - function that cheks .
+ * _sqrt_recursion_internal - function that
+ * cheks if i is the square root of n.
+ * @n: input data
+ * @i: cheker for square root
+ *
+ * Return: the square root i.
+ */
+int _sqrt_recursion_internal(int n, int i)
+{
+	if (i * i == n)
+	{
+		return (i);
+	}
+	if (i * i > n)
+	{
+		return (-1);
+	}
+	return (_sqrt_recursion_internal(n, i + 1));
+}
+/**
+ * _sqrt_recursion - function that returns the
+ * natural square root of a number .
  * @n: input data
  *
  * Return: square root of n.
  */
 int _sqrt_recursion(int n)
 {
-	static int i;
-
 	if (n < 0)
 	{
-		i = 0;
 		return (-1);
 	}
-	if (i * i == n)
-	{
-		int result = i;
-
-		i = 0;
-		return (result);
-	}
-	if (i * i > n)
-	{
-		i = 0;
-		return (-1);
-	}
-	i++;
-	return (_sqrt_recursion(n));
+	return (_sqrt_recursion_internal(n, 0));
 }
