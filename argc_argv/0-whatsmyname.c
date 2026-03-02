8852a12feb2e9bb89fcf50098e5bdf90b8fe9c0c
maryem <zanned.maryem@gmail.com>
2026-03-02 14:52:08 +0100

    add 0-whatsmyname.c main.h README.md files

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+/**
+ *main - a program that prints its name, followed by a new line.
+ *@argc: number of arguments passed to the program
+ *@argv: array of arguments passd to the program
+ *
+ * Return: 0 Success.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
