f2011758c7bd51261dc394643837d95578f5bf1f
maryem <zanned.maryem@gmail.com>
2026-03-05 14:31:27 +0100

    add 3-alloc_grid.c file with free

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -22,7 +22,7 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < width; i++)
+	for (i = 0; i < height; i++)
 	{
 		array[i] = malloc(width * sizeof(int));
 		if (array[i] == NULL)
@@ -34,7 +34,7 @@ int **alloc_grid(int width, int height)
 			free(array);
 			return (NULL);
 		}
-		for (j = 0; j < height; j++)
+		for (j = 0; j < width; j++)
 		{
 			array[i][j] = 0;
 		}
