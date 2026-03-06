b630b1bb9e7f6d54fa4d711028fabefc5a5cfde8
maryem <zanned.maryem@gmail.com>
2026-03-06 14:51:45 +0100

    add 2-calloc.c file with corrections

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -22,10 +22,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	{
 		return (NULL);
 	}
-	for (i = 0; array[i] < nmemb; i++)
+	for (i = 0; i < nmemb * size; i++)
 	{
 		array[i] = 0;
-		i++;
 	}
 	return (array);
 }
