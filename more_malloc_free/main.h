419cfae4498dcd0efd4076805ca7fd16f4687ff1
maryem <zanned.maryem@gmail.com>
2026-03-06 13:55:51 +0100

    adding updates et main.h

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -8,6 +8,9 @@
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c);
+int _strlen(char *s);
+char *s_check(char *s);
+char *_strncat(char *s1, char *s2, unsigned int n);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
