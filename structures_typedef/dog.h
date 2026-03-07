0716ff92656765b59c0c5d57ae5e3cc375c327ae
maryem <zanned.maryem@gmail.com>
2026-03-07 14:57:44 +0100

    adding dog.h file

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure for dog informations
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner.
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
