bc44f401ee100923c18c8428de8481a2125dade5
maryem <zanned.maryem@gmail.com>
2026-03-08 16:04:07 +0100

    add 4-new_dog.c file

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,5 @@
 #include "dog.h"
 #include <stdlib.h>
-
 /**
  * _strlen - returns the length of a string
  * @s: string to measure
@@ -9,13 +8,12 @@
  */
 int _strlen(char *s)
 {
-    int len = 0;
+	int len = 0;
 
-    while (s[len] != '\0')
-        len++;
-    return len;
+	while (s[len] != '\0')
+		len++;
+	return len;
 }
-
 /**
  * _strcpy - copies a string to another buffer
  * @dest: destination buffer
@@ -25,17 +23,16 @@ int _strlen(char *s)
  */
 char *_strcpy(char *dest, char *src)
 {
-    int i = 0;
+	int i = 0;
 
-    while (src[i] != '\0')
-    {
-        dest[i] = src[i];
-        i++;
-    }
-    dest[i] = '\0';
-    return dest;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return dest;
 }
-
 /**
  * new_dog - creates a new dog
  * @name: dog's name
@@ -46,37 +43,37 @@ char *_strcpy(char *dest, char *src)
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *d;
-    int len_name, len_owner;
+	dog_t *d;
+	int len_name, len_owner;
 
-    if (name == NULL || owner == NULL)
-        return NULL;
+	if (name == NULL || owner == NULL)
+		return NULL;
 
-    d = malloc(sizeof(dog_t));
-    if (d == NULL)
-        return NULL;
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
+		return NULL;
 
-    len_name = _strlen(name);
-    len_owner = _strlen(owner);
+	len_name = _strlen(name);
+	len_owner = _strlen(owner);
 
-    d->name = malloc(len_name + 1);
-    if (d->name == NULL)
-    {
-        free(d);
-        return NULL;
-    }
-    _strcpy(d->name, name);
+	d->name = malloc(len_name + 1);
+	if (d->name == NULL)
+	{
+		free(d);
+		return NULL;
+	}
+	_strcpy(d->name, name);
 
-    d->owner = malloc(len_owner + 1);
-    if (d->owner == NULL)
-    {
-        free(d->name);
-        free(d);
-        return NULL;
-    }
-    _strcpy(d->owner, owner);
+	d->owner = malloc(len_owner + 1);
+	if (d->owner == NULL)
+	{
+		free(d->name);
+		free(d);
+		return NULL;
+	}
+	_strcpy(d->owner, owner);
 
-    d->age = age;
+	d->age = age;
 
-    return d;
+	return d;
 }
