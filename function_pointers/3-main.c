9deffcb81d6c642b714b98ad293574c84e000fb6
maryem <zanned.maryem@gmail.com>
2026-03-10 23:41:58 +0100

    uploading calc files

---
+++ function_pointers/3-main.c
@@ -0,0 +1,33 @@
+#include"3-calc.h"
+#include<stdlib.h>
+#include<stdio.h>
+#include <string.h>
+/**
+ * 
+ * 
+ * 
+ * 
+ */
+int main(int argc, char *argv[])
+{
+    int (*func)(int, int);
+
+    if (argc != 4)
+    {
+        printf("Error\n");
+        exit(98);
+    }
+    func = get_op_func(argv[2]);
+    if (func == NULL)
+    {
+        printf("Error\n");
+        return (99);
+    }
+    if ((strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0) && atoi(argv[3]) == 0)
+    {
+        printf("Error\n");
+        exit(100);
+    }
+    printf("%d\n",func(atoi(argv[1]),atoi(argv[3])));
+    return(0);
+}
