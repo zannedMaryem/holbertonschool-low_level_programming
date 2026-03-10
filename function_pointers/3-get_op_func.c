b358b91a614abc01633bd1bfd413ea1f2f425c44
maryem <zanned.maryem@gmail.com>
2026-03-10 23:52:36 +0100

    uploading calc files and updates with betty style requirments

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,16 +1,17 @@
 #include "3-calc.h"
-#include<stdlib.h>
-#include<string.h>
+#include <stdlib.h>
+#include <string.h>
 /**
- *get_op_func
+ *get_op_func - unction that selects the correct function
+ *to perform the operation asked by the user
+ *@s: operator + or - or * or / or %
  *
- *
- *
+ *Return: returns a pointer to the function that corresponds
+ * to the operator given as a parameter..
  */
 int (*get_op_func(char *s))(int, int)
 {
-	op_t ops[] =
-	{
+	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
 		{"*", op_mul},
@@ -18,13 +19,13 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}};
 	int i = 0;
-	
-    while (ops[i].op != NULL)
-    {
-        if (strcmp(ops[i].op, s) == 0)
-            return (ops[i].f);
-        i++;
-    }
 
-    return (NULL);
+	while (ops[i].op != NULL)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);
+		i++;
+	}
+
+	return (NULL);
 }
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,33 +1,35 @@
-#include"3-calc.h"
-#include<stdlib.h>
-#include<stdio.h>
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 /**
- * 
- * 
- * 
- * 
+ *main - calculator project
+ *@argc: number of arguments passed to the program.
+ *@argv: arguments
+ *
+ * Return: Success 0
  */
 int main(int argc, char *argv[])
 {
-    int (*func)(int, int);
+	int (*func)(int, int);
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        exit(98);
-    }
-    func = get_op_func(argv[2]);
-    if (func == NULL)
-    {
-        printf("Error\n");
-        return (99);
-    }
-    if ((strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0) && atoi(argv[3]) == 0)
-    {
-        printf("Error\n");
-        exit(100);
-    }
-    printf("%d\n",func(atoi(argv[1]),atoi(argv[3])));
-    return(0);
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	func = get_op_func(argv[2]);
+	if (func == NULL)
+	{
+		printf("Error\n");
+		return (99);
+	}
+	if ((strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0)
+		&& atoi(argv[3]) == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", func(atoi(argv[1]), atoi(argv[3])));
+	return (0);
 }
