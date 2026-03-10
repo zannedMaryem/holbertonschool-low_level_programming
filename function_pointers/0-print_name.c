9cd7a79f4b2cd18f55c8e01e916002848afdb481
maryem <zanned.maryem@gmail.com>
2026-03-10 15:44:58 +0100

    correcting file with betty style

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,17 +1,17 @@
-#include"function_pointers.h"
-#include<stdlib.h>
+#include "function_pointers.h"
+#include <stdlib.h>
 /**
  * print_name - a function that prints a name.
  * @name: name to print
  * @f: pointer to a function that takes a char* and returns void
- * 
+ *
  * Return : nothing.
  */
 void print_name(char *name, void (*f)(char *))
 {
-    if ( name == NULL || f == NULL)
-    {
-        return;
-    }
-    f(name);
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
+	f(name);
 }
