49b80351dce987a90ee6ca0e6727f4e6067844fa
maryem <zanned.maryem@gmail.com>
2026-03-10 16:04:33 +0100

    add function_pointers.h with updates

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
+#include <stddef.h>
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -9,4 +10,5 @@
  */
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
