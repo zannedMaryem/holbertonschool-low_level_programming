a97056c5076a6550b83cdf429532e25492da943e
maryem <zanned.maryem@gmail.com>
2026-03-14 15:09:59 +0100

    adding update to 2-print_strings.c file

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -19,7 +19,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		string = va_arg(ap, char *);
 		if (string == NULL)
 		{
-			printf("nil");
+			printf("(nil)");
 		}
 		else
 		{
