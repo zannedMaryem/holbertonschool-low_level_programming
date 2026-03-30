d1c0791f887c89b064f2b324c7b3cced0128d119
maryem <zanned.maryem@gmail.com>
2026-03-30 14:46:14 +0100

    add 3-add_node_end.c, _strlen.c and lists.h files

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -37,3 +37,18 @@ list_t *add_node_end(list_t **head, const char *str)
 	ptr->next = tmp;
 	return (tmp);
 }
+/**
+ * _strlen - calculates the length of a string
+ * @s: input string
+ * Return: string length.
+ */
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
