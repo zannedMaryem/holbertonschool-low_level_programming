1c880a9f095b333535d6615d392b8f4d0d779919
maryem <zanned.maryem@gmail.com>
2026-03-31 10:56:43 +0100

    adding updates to 2-add_dnodeint.c file

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -19,6 +19,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	tmp->n = n;
 	tmp->next = NULL;
 	tmp->next = *head;
+	if (*head != NULL)
+	{
+		(*head)->prev = tmp;
+	}
 	*head = tmp;
 	return (*head);
 }
