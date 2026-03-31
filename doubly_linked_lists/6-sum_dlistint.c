a24d6c5b7c25e6ce92d378f5bf9389ad1ef9e9e5
maryem <zanned.maryem@gmail.com>
2026-03-31 21:54:24 +0100

    add 6-sum_dlistint.c file

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * sum_dlistint - a function that finds a node at
+ * index.
+ * @head: the head of the linked list.
+ * @index: index of the node.
+ * Return: the adress of the node at index or NULL if not found.
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
