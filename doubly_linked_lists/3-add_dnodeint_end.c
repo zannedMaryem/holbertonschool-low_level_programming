745703270ca580e443156b07c7b156160ffe925a
maryem <zanned.maryem@gmail.com>
2026-03-31 14:30:10 +0100

    adding updates to 3-add_dnodeint_end.c file

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -24,6 +24,10 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		*head = tmp;
 		return (tmp);
 	}
+	else if (*head != NULL)
+	{
+		(*head)->prev = tmp;
+	}
 	ptr = *head;
 	while (ptr->next != NULL)
 	{
