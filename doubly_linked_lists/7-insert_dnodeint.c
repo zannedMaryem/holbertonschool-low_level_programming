72016b7e6fe61ea549d75bc408aa7b78c940127e
maryem <zanned.maryem@gmail.com>
2026-04-01 17:23:37 +0100

    add 7-insert_dnodeint.c file

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -11,44 +11,44 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *pos;
-    dlistint_t *new = malloc(sizeof(dlistint_t));
-    unsigned int i = 0;
+	dlistint_t *pos;
+	dlistint_t *new = malloc(sizeof(dlistint_t));
+	unsigned int i = 0;
 
-    if (new == NULL)
-    {
-        return (NULL);
-    }
-    new->prev = NULL;
-    new->n = n;
-    new->next = NULL;
-    if (idx == 0)
-    {
-        new->next = *h;
-        if (*h != NULL)
-        {
-            (*h)->prev = new;
-        }
-        *h = new;
-        return (new);
-    }
-    pos = *h;
-    while (pos != NULL && i < idx - 1)
-    {
-        pos = pos->next;
-        i++;
-    }
-    if (pos == NULL)
-    {
-        free(new);
-        return (NULL);
-    }
-    new->next = pos->next;
-    new->prev = pos;
-    if (pos->next != NULL)
-    {
-        pos->next->prev = new;
-    }
-    pos->next = new;
-    return (new);
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->prev = NULL;
+	new->n = n;
+	new->next = NULL;
+	if (idx == 0)
+	{
+		new->next = *h;
+		if (*h != NULL)
+		{
+			(*h)->prev = new;
+		}
+		*h = new;
+		return (new);
+	}
+	pos = *h;
+	while (pos != NULL && i < idx - 1)
+	{
+		pos = pos->next;
+		i++;
+	}
+	if (pos == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->next = pos->next;
+	new->prev = pos;
+	if (pos->next != NULL)
+	{
+		pos->next->prev = new;
+	}
+	pos->next = new;
+	return (new);
 }
