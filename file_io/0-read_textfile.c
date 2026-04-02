b7e8e8f3707c515706cf80a693c7143ae777df9c
maryem <zanned.maryem@gmail.com>
2026-04-02 23:21:06 +0100

    add 0-read_textfile.c file updated

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -17,25 +17,33 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t o_err, r_err, w_err;
-	size_t len = 0;
 	char *msg = malloc(sizeof(*msg) * letters);
 
 	if (letters == 0 || msg == NULL || filename == NULL)
 	{
 		return (0);
 	}
-	o_err = open(filename, O_RDONLY, S_IRUSR | S_IWUSR);
-	r_err = read(o_err, msg, letters);
-	if (o_err < 0 || r_err < 0)
+	o_err = open(filename, O_RDONLY);
+	if ( o_err < 0)
 	{
+		close(o_err);
+		return(0);
+	}
+	r_err = read(o_err, msg, letters);
+	if (r_err < 0)
+	{
+		close(o_err);
+		free(msg);
 		return (0);
 	}
-	w_err = write(STDOUT_FILENO, msg, letters);
+	w_err = write(STDOUT_FILENO, msg, r_err);
 	if (w_err < 0)
 	{
+		close(o_err);
+		free(msg);
 		return (0);
 	}
 	close(o_err);
-	len = strlen(msg);
-	return (len);
+	free(msg);
+	return (w_err);
 }
