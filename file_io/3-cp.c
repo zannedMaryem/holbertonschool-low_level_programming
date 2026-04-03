01fa06880615bcd7ecf49ba2ce8562016dfbfcc3
maryem <zanned.maryem@gmail.com>
2026-04-03 14:34:22 +0100

    add 3-cp.c file updated with close at the right location

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -14,7 +14,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int o_fd1, o_fd2, r_fd, w_fd, c_fd1, c_fd2;
+	int o_fd1, o_fd2, r_fd, w_fd;
 	char buffer[1024];
 
 	if (argc != 3)
@@ -26,24 +26,12 @@ int main(int argc, char *argv[])
 	if (o_fd1 < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		c_fd1 = close(o_fd1);
-		if (c_fd1 < 0)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", c_fd1);
-			exit(100);
-		}
 		exit(98);
 	}
 	o_fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (o_fd2 < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		c_fd2 = close(o_fd2);
-		if (c_fd2 < 0)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", c_fd2);
-			exit(100);
-		}
 		exit(99);
 	}
 	while ((r_fd = read(o_fd1, buffer, sizeof(buffer))) > 0)
@@ -51,28 +39,24 @@ int main(int argc, char *argv[])
 		if (r_fd < 0)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			c_fd1 = close(o_fd1);
-			if (c_fd1 < 0)
-			{
-				dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", c_fd1);
-				exit(100);
-			}
 			exit(98);
 		}
 		w_fd = write(o_fd2, buffer, r_fd);
 		if (w_fd < 0)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			c_fd2 = close(o_fd2);
-			if (c_fd2 < 0)
-			{
-				dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", c_fd2);
-				exit(100);
-			}
 			exit(99);
 		}
 	}
-	close(o_fd1);
-	close(o_fd2);
+	if (close(o_fd1) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", o_fd1);
+		exit(100);
+	}
+	if (close(o_fd2) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", o_fd2);
+		exit(100);
+	}
 	return (0);
 }
