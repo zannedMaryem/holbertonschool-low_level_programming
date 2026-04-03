344e8956d4e20ff1eaecf2aac7846fb1784f129b
maryem <zanned.maryem@gmail.com>
2026-04-03 01:16:45 +0100

    add 1-create_file.c file and update main.h

---
+++ file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+/**
+ * read_textfile - function that reads a text file
+ * and prints it to the POSIX standard output
+ * @filename: the file name.
+ * @letters: number of letters to be read and written.
+ *
+ * Return: returns the actual number of letters it could
+ * read and print or 0 if filename is NULL or it fails
+ * to open, read or write the file.
+ */
+int create_file(const char *filename, char *text_content)
+{
+    int c_fd, w_fd;
+
+    if (filename == NULL)
+    {
+        return(-1);
+    }
+    c_fd = creat(filename, S_IRUSR | S_IWUSR);
+    if (c_fd < 0)
+    {
+        close(c_fd);
+        return(-1);
+    }
+    w_fd = write(STDOUT_FILENO, text_content, strlen(text_content));
+    if (w_fd < 0)
+    {
+        close(c_fd);
+        return(-1);
+    }
+    close(c_fd);
+    return(1);
+}
