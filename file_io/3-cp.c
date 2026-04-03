6117ece6dacfbe1f555499190faecc1f79893b6e
maryem <zanned.maryem@gmail.com>
2026-04-03 14:23:38 +0100

    add 3-cp.c file and changed permissions to destination file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -34,7 +34,7 @@ int main(int argc, char *argv[])
 		}
 		exit(98);
 	}
-	o_fd2 = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	o_fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (o_fd2 < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
