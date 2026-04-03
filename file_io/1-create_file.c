8fa37675901dbb884bf78dc424337c1f369de149
maryem <zanned.maryem@gmail.com>
2026-04-03 01:26:46 +0100

    add 1-create_file.c file with modifications

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -26,7 +26,10 @@ int create_file(const char *filename, char *text_content)
 		close(c_fd);
 		return (-1);
 	}
-	w_fd = write(c_fd, text_content, strlen(text_content));
+	if (text_content != NULL)
+	{
+		w_fd = write(c_fd, text_content, strlen(text_content));
+	}
 	if (w_fd < 0)
 	{
 		close(c_fd);
