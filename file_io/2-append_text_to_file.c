1afaff2261d422a06ad3f1433ff86615450b591d
maryem <zanned.maryem@gmail.com>
2026-04-03 01:54:03 +0100

    add 2-append_text_to_file.c file and updates on 1-create_file.c file

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -5,36 +5,36 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 /**
- * create_file - function that creates a text file
- * and witres a test in it.
+ * append_text_to_file - function that appends
+ * a text to an existing file.
  * @filename: the file name.
  * @text_content: text to write.
  *
  * Return: returns 1 in succuss or -1 if failed.
  */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
-	int c_fd, w_fd;
+	int o_fd, w_fd;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
-	c_fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (c_fd < 0)
+	o_fd = open(filename, O_WRONLY | O_APPEND);
+	if (o_fd < 0)
 	{
-		close(c_fd);
+		close(o_fd);
 		return (-1);
 	}
 	if (text_content != NULL)
 	{
-		w_fd = write(c_fd, text_content, strlen(text_content));
+		w_fd = write(o_fd, text_content, strlen(text_content));
 	}
 	if (w_fd < 0)
 	{
-		close(c_fd);
+		close(o_fd);
 		return (-1);
 	}
-	close(c_fd);
+	close(o_fd);
 	return (1);
 }
