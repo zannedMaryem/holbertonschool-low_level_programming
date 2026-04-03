f0ec05385e989a5a9fc6e2d171243b0f0cf21c42
maryem <zanned.maryem@gmail.com>
2026-04-03 01:24:45 +0100

    add 1-create_file.c file with modifications

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -5,35 +5,33 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 /**
- * read_textfile - function that reads a text file
+ * create_file - function that reads a text file
  * and prints it to the POSIX standard output
  * @filename: the file name.
- * @letters: number of letters to be read and written.
+ * @text_content: text to write.
  *
- * Return: returns the actual number of letters it could
- * read and print or 0 if filename is NULL or it fails
- * to open, read or write the file.
+ * Return: returns 1 in succuss or -1 if failed.
  */
 int create_file(const char *filename, char *text_content)
 {
-    int c_fd, w_fd;
+	int c_fd, w_fd;
 
-    if (filename == NULL)
-    {
-        return(-1);
-    }
-    c_fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (c_fd < 0)
-    {
-        close(c_fd);
-        return(-1);
-    }
-    w_fd = write(c_fd, text_content, strlen(text_content));
-    if (w_fd < 0)
-    {
-        close(c_fd);
-        return(-1);
-    }
-    close(c_fd);
-    return(1);
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	c_fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (c_fd < 0)
+	{
+		close(c_fd);
+		return (-1);
+	}
+	w_fd = write(c_fd, text_content, strlen(text_content));
+	if (w_fd < 0)
+	{
+		close(c_fd);
+		return (-1);
+	}
+	close(c_fd);
+	return (1);
 }
