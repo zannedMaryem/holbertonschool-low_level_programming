e9684593c89bb41d771eabdb2051769f3caaf55c
maryem <zanned.maryem@gmail.com>
2026-04-03 01:22:15 +0100

    add 1-create_file.c file with modifications

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -22,13 +22,13 @@ int create_file(const char *filename, char *text_content)
     {
         return(-1);
     }
-    c_fd = creat(filename, S_IRUSR | S_IWUSR);
+    c_fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
     if (c_fd < 0)
     {
         close(c_fd);
         return(-1);
     }
-    w_fd = write(STDOUT_FILENO, text_content, strlen(text_content));
+    w_fd = write(c_fd, text_content, strlen(text_content));
     if (w_fd < 0)
     {
         close(c_fd);
