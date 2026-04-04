b8f70a5c24b9b8ef760ef14c066fbe764920fd8a
maryem <zanned.maryem@gmail.com>
2026-04-04 14:51:14 +0100

    add 1-create_file.c file with updates

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -29,11 +29,11 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		w_fd = write(c_fd, text_content, strlen(text_content));
-	}
-	if (w_fd < 0)
-	{
-		close(c_fd);
-		return (-1);
+		if (w_fd < 0)
+		{
+			close(c_fd);
+			return (-1);
+		}
 	}
 	close(c_fd);
 	return (1);
