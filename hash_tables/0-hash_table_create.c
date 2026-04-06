fd7738ca0439b08f307ad74984413f650d9db525
maryem <zanned.maryem@gmail.com>
2026-04-06 20:17:49 +0100

    add starting files

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * hash_table_create - Creates a hash table
+ * @size: The size of the hash table
+ *
+ * Return: A pointer to the newly created hash table, or NULL if it fails
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    hash_table_t *tab;
+
+    if (size == 0)
+    {
+        return(NULL);
+    }
+    tab = malloc(sizeof(hash_table_t)* size);
+    if (tab == NULL)
+    {
+        return(NULL);
+    }
+    tab->size = size;
+    
+    return(tab);
+}
---
+++ hash_tables/README.md
@@ -0,0 +1,3 @@
+# Hash tables
+
+## This directory contains basic functions that help understand how to use and manipulate Hash tables and Hash functions
