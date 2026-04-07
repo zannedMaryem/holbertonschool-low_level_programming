2dd61abd4ab74042c22de640856fad7ed162ed1e
maryem <zanned.maryem@gmail.com>
2026-04-07 14:32:13 +0100

    add 6-hash_table_delete.c file with updates to betty style doc

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -1,32 +1,30 @@
 #include "hash_tables.h"
 #include <stdlib.h>
 /**
- * 
- * 
- * 
- * 
+ * hash_table_delete - function that deletes a hash table.
+ * @ht: hash table to delete.
  */
 void hash_table_delete(hash_table_t *ht)
 {
-    hash_node_t *ptr, *tmp;
-    unsigned long int i;
+	hash_node_t *ptr, *tmp;
+	unsigned long int i;
 
-    if (ht == NULL)
-    {
-        return;
-    }
-    for (i = 0; i < ht->size; i++)
-    {
-        ptr = ht->array[i];
-        while (ptr != NULL)
-        {
-            tmp = ptr->next;
-            free(ptr->key);
-            free(ptr->value);
-            free(ptr);
-            ptr = tmp;
-        }
-    }
-    free(ht->array);
-    free(ht);
+	if (ht == NULL)
+	{
+		return;
+	}
+	for (i = 0; i < ht->size; i++)
+	{
+		ptr = ht->array[i];
+		while (ptr != NULL)
+		{
+			tmp = ptr->next;
+			free(ptr->key);
+			free(ptr->value);
+			free(ptr);
+			ptr = tmp;
+		}
+	}
+	free(ht->array);
+	free(ht);
 }
