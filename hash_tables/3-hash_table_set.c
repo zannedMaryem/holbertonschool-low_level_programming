f73691f9a491e4e5c6beff3c9d9227192325848b
maryem <zanned.maryem@gmail.com>
2026-04-07 12:51:50 +0100

    add 3-hash_table_set.c file

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,41 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * hash_table_set - function that adds an element to the hash table.
+ * @ht: hash table.
+ * @key: key of hash function.
+ * @value: value to assign to the hash table.
+ * Return : 1 if it succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new_node, *head;
+
+	if (ht == NULL || key == NULL)
+	{
+		return (0);
+	}
+	index = key_index((const unsigned char *)key, ht->size);
+	head = ht->array[index];
+	while (head != NULL)
+	{
+		if (strcmp(head->key, key) == 0)
+		{
+			head->value = strdup(value);
+			return (1);
+		}
+		head = head->next;
+	}
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+	{
+		return (0);
+	}
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
+}
